ca58cc68d20fa27918fb85856d04fd5734fc7490
Blooossom <taegyeong19@gmail.com>
2023-04-11 20:13:04 +0900

    feat : 회원 Role Enum 설정 - #2

---
+++ src/main/java/com/shortping/param/Role.java
@@ -0,0 +1,16 @@
+package com.shortping.param;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Role {
+
+    ROLE_SELLER,
+
+    ROLE_BUYER,
+
+    ROLE_ADMIN
+
+}
