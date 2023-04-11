7e9faade4cd26273c9a5a60b051dc6b17ba02f7f
Blooossom <taegyeong19@gmail.com>
2023-04-11 19:49:23 +0900

    Initial commit

---
+++ src/main/java/com/shortping/ShortpingApplication.java
@@ -0,0 +1,13 @@
+package com.shortping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortpingApplication.class, args);
+	}
+
+}
