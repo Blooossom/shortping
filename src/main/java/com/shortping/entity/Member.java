8c6d1d908f2676f707d9f1e4704faab1c319d9fb
Blooossom <taegyeong19@gmail.com>
2023-04-11 20:12:30 +0900

    feat : 회원 Entity 작성 - #2

---
+++ src/main/java/com/shortping/entity/Member.java
@@ -0,0 +1,49 @@
+package com.shortping.entity;
+
+
+import com.shortping.param.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "member")
+public class Member {
+
+    @Id @Column(name = "member_email")
+    private String memberEmail;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "address_detail")
+    private String addressDetail;
+
+    @Column(name = "birth")
+    private String birth;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "member_delete")
+    private boolean memberDelete;
+    
+
+
+}
