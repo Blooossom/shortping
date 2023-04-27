ed42cf4a65b24921298c0b2e3311065245d7a70e
Blooossom <taegyeong19@gmail.com>
2023-04-27 20:05:33 +0900

    conf : SecurityConfig update - #4

--- src/main/java/com/shortping/config/SecurityConfig.java
+++ src/main/java/com/shortping/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,6 +31,8 @@ import java.util.List;
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
+
+
     private final JWTManager jwtManager;
 
     @Autowired
@@ -37,7 +40,8 @@ public class SecurityConfig {
 
     private static final String[] PUBLIC_URLS = {
             "/auth/general/signup", "/auth/general/login", "/auth/general/send_number", "/auth/general/check_number",
-            "/auth/general/find_email", "/auth/general/find_password", "/auth/general/reset_password"
+            "/auth/general/find_email", "/auth/general/find_password", "/auth/general/reset_password", "auth/general/test",
+            "/item/**"
     };
 
     @Bean
@@ -49,21 +53,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)throws Exception {
         return http
-                .authorizeRequests()
-                .mvcMatchers(PUBLIC_URLS).permitAll()
+                .authorizeRequests()// 다음 리퀘스트에 대한 사용권한 체크
+                .mvcMatchers(PUBLIC_URLS).permitAll() // 가입 및 인증 주소는 누구나 접근가능
                 .and()
-                .authorizeRequests()
-                .anyRequest().authenticated()
+                .authorizeRequests()// 다음 리퀘스트에 대한 사용권한 체크
+                .anyRequest().authenticated()// 그외 나머지 요청은 모두 인증된 회원만 접근 가능
                 .and()
                 .exceptionHandling()
-                .authenticationEntryPoint(customAuthEntityPoint)
+                .authenticationEntryPoint(customAuthEntityPoint)     // 토큰없이 접근하는 경우에 대해 Exception 발생
                 .and()
                 .cors()
                 .configurationSource(corsConfigurationSource())
                 .and()
-                .csrf().disable()
-                .httpBasic().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .csrf().disable()       // rest api이므로 csrf 보안이 필요없으므로 disable처리
+                .httpBasic().disable()  // 기본설정 사용안함. 기본설정은 비인증시 로그인폼 화면으로 리다이렉트 된다.
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt token으로 인증할것이므로 세션필요없으므로 생성안함.
                 .and()
                 .addFilterBefore(JWTFilter.of(jwtManager), UsernamePasswordAuthenticationFilter.class)
                 .build();
