61f82a5010807e577a6618cad2975a96ed72e318
Blooossom <taegyeong19@gmail.com>
2023-04-27 20:20:46 +0900

    feat : 장바구니 조회 관련 DTO 추가 - #18

--- src/main/java/com/shortping/dto/cart/CartRes.java
+++ src/main/java/com/shortping/dto/cart/CartRes.java
@@ -1,5 +1,6 @@
 package com.shortping.dto.cart;
 
+import com.shortping.entity.Cart;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,6 +17,26 @@ public class CartRes {
         private String itemName;
 
         private LocalDateTime addAt;
+    }
+
+    @Getter @Setter
+    public static class CartList {
+
+        private Long id;
+
+        private String itemName;
+
+        private Long itemPrice;
+
+        private LocalDateTime addAt;
+
+
+        public CartList(Cart cart) {
+            this.id = cart.getId();
+            this.itemName = cart.getItem().getItemName();
+            this.itemPrice = cart.getItem().getItemPrice();
+            this.addAt = cart.getAddAt();
+        }
     }
 
 
